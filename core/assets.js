d32676b029670bb7e2414938a036d2ff12e9945a
DrHoschi <mann_alexander2000@yahoo.de>
2025-08-12 11:54:56 +0200

    Update assets.js

--- core/assets.js
+++ core/assets.js
@@ -1,1 +1,22 @@
-neuer code
+// Lädt optionale Texturen. Wenn eine fehlt, wird im Renderer ein Farb-Fallback benutzt.
+export const IM = {
+  grass:null, water:null, shore:null
+};
+const LIST = [
+  ['grass','assets/grass.png'],
+  ['water','assets/water.png'],
+  ['shore','assets/shore.png'],
+];
+
+export function loadAllAssets(){
+  return Promise.all(LIST.map(([k,src])=> loadOne(k,src)));
+}
+
+function loadOne(key, src){
+  return new Promise(res=>{
+    const img=new Image();
+    img.onload=()=>{ IM[key]=img; res(); };
+    img.onerror=()=>{ console.warn('[assets] fehlend:', src); IM[key]=null; res(); };
+    img.src=src;
+  });
+}
