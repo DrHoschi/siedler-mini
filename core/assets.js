99ea6f19025df00086db5e5d37f640697db59b8d
DrHoschi <mann_alexander2000@yahoo.de>
2025-08-13 13:54:53 +0200

    Update assets.js

--- core/assets.js
+++ core/assets.js
@@ -1,36 +1,36 @@
 // V14.1 – Assets laden (PNG aus /assets, sonst Platzhalter)
 export const IM = {
-  grass:null, water:null, shore:null, dirt:null, rocky:null, sand:null, road:null,
-  road_straight:null, road_curve:null,
-  hq_stone:null, hq_wood:null, lumberjack:null, depot:null, carrier:null
+  grass: null, water: null, shore: null, dirt: null, rocky: null, sand: null,
+  road: null, road_straight: null, road_curve: null,
+  hq_stone: null, hq_wood: null, lumberjack: null, depot: null, carrier: null
 };
 
 const MAP = [
-  ['grass','assets/grass.png'],
-  ['water','assets/water.png'],
-  ['shore','assets/shore.png'],
-  ['dirt','assets/dirt.png'],
-  ['rocky','assets/rocky.png'],
-  ['sand','assets/sand.png'],
-  ['road','assets/road.png'],
-  ['road_straight','assets/road_straight.png'],
-  ['road_curve','assets/road_curve.png'],
-  ['hq_stone','assets/hq_stone.png'],
-  ['hq_wood','assets/hq_wood.png'],
-  ['lumberjack','assets/lumberjack.png'],
-  ['depot','assets/depot.png'],
-  ['carrier','assets/carrier.png'],
+  ['grass',         'assets/grass.png'],
+  ['water',         'assets/water.png'],
+  ['shore',         'assets/shore.png'],
+  ['dirt',          'assets/dirt.png'],
+  ['rocky',         'assets/rocky.png'],
+  ['sand',          'assets/sand.png'],
+  ['road',          'assets/road.png'],
+  ['road_straight', 'assets/road_straight.png'],
+  ['road_curve',    'assets/road_curve.png'],
+  ['hq_stone',      'assets/hq_stone.png'],
+  ['hq_wood',       'assets/hq_wood.png'],
+  ['lumberjack',    'assets/lumberjack.png'],
+  ['depot',         'assets/depot.png'],
+  ['carrier',       'assets/carrier.png'],
 ];
 
 export async function loadAllAssets(){
-  await Promise.all(MAP.map(([k,src])=>loadOne(k,src)));
+  await Promise.all(MAP.map(([k, src]) => loadOne(k, src)));
 }
 
-function loadOne(k,src){
-  return new Promise(res=>{
-    const img=new Image();
-    img.onload=()=>{ IM[k]=img; res(); };
-    img.onerror=()=>{ console.warn(`[assets] fehlt: ${src}`); IM[k]=null; res(); };
-    img.src=src;
+function loadOne(key, src){
+  return new Promise(res => {
+    const img = new Image();
+    img.onload  = () => { IM[key] = img; res(); };
+    img.onerror = () => { console.warn(`[assets] fehlt: ${src}`); IM[key] = null; res(); };
+    img.src = src;
   });
 }
