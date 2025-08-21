ab28bc6d2f25085868cdd3ea212689935b6b2df8
DrHoschi <mann_alexander2000@yahoo.de>
2025-08-22 01:31:19 +0200

    Create items.js

---
+++ assets/items/items.js
@@ -0,0 +1,51 @@
+{
+  "meta": {
+    "image": "items_sprite_sheet.png",
+    "size": {"w": 768, "h": 768},
+    "scale": 1
+  },
+  "frames": {
+    "item_log": {
+      "frame": {"x": 0, "y": 0, "w": 256, "h": 256},
+      "rotated": false,
+      "trimmed": false,
+      "sourceSize": {"w": 256, "h": 256},
+      "spriteSourceSize": {"x": 0, "y": 0, "w": 256, "h": 256}
+    },
+    "item_stone": {
+      "frame": {"x": 256, "y": 0, "w": 256, "h": 256},
+      "rotated": false,
+      "trimmed": false,
+      "sourceSize": {"w": 256, "h": 256},
+      "spriteSourceSize": {"x": 0, "y": 0, "w": 256, "h": 256}
+    },
+    "item_sack": {
+      "frame": {"x": 512, "y": 0, "w": 256, "h": 256},
+      "rotated": false,
+      "trimmed": false,
+      "sourceSize": {"w": 256, "h": 256},
+      "spriteSourceSize": {"x": 0, "y": 0, "w": 256, "h": 256}
+    },
+    "item_crate": {
+      "frame": {"x": 0, "y": 256, "w": 256, "h": 256},
+      "rotated": false,
+      "trimmed": false,
+      "sourceSize": {"w": 256, "h": 256},
+      "spriteSourceSize": {"x": 0, "y": 0, "w": 256, "h": 256}
+    },
+    "item_bucket_full": {
+      "frame": {"x": 256, "y": 256, "w": 256, "h": 256},
+      "rotated": false,
+      "trimmed": false,
+      "sourceSize": {"w": 256, "h": 256},
+      "spriteSourceSize": {"x": 0, "y": 0, "w": 256, "h": 256}
+    },
+    "item_food_bundle": {
+      "frame": {"x": 512, "y": 256, "w": 256, "h": 256},
+      "rotated": false,
+      "trimmed": false,
+      "sourceSize": {"w": 256, "h": 256},
+      "spriteSourceSize": {"x": 0, "y": 0, "w": 256, "h": 256}
+    }
+  }
+}
