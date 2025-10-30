f0869abb5b5e8e2875564bfcbf7fb5ee4abc531e
DrHoschi <mann_alexander2000@yahoo.de>
2025-10-30 19:39:01 +0100

    Create inspector.tab.paths-v1.js

---
+++ inspector/tabs/inspector.tab.paths-v1.js
@@ -0,0 +1,6 @@
+(() => {
+  document.addEventListener("DOMContentLoaded",()=>{
+    const div=document.getElementById("insp-paths");
+    if(div) div.innerHTML="<p>Pfade-Tools folgen hier.</p>";
+  });
+})();
