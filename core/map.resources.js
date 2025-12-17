3ca7edb4d0346e6c2c913c705308e422ff2a00d1
DrHoschi <mann_alexander2000@yahoo.de>
2025-12-17 19:39:21 +0100

    Update map.resources.js

--- core/map.resources.js
+++ core/map.resources.js
@@ -34,8 +34,8 @@
 
   // Basismengen (Start-Sandbox)
   const CFG = {
-    trees: { count: 240, clusterChance: 0.45 },
-    stones:{ count: 118, clusterChance: 0.35 },
+    trees: { count: 140, clusterChance: 0.45 },
+    stones:{ count: 68, clusterChance: 0.35 },
     fish:  { count: 22, clusterChance: 0.55 },
 
     // Zeichnungs-Skalierung relativ zu tileSize:
