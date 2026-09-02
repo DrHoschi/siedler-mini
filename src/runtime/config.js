6e8c62c8f21218eda294187628ec179b7e22fcb5
DrHoschi <mann_alexander2000@yahoo.de>
2026-09-02 23:21:22 +0200

    fix(cr-04b): set visible build identifier

--- src/runtime/config.js
+++ src/runtime/config.js
@@ -1,6 +1,6 @@
 export const RuntimeConfig = Object.freeze({
   product: 'Neue Siedler',
-  build: 'CR-04A',
+  build: 'CR-04B',
   simulation: Object.freeze({
     fixedStepMs: 100,
     maxCatchUpSteps: 4,
