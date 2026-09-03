1a9692b52eb006e582fd27651f70944bf4235af0
DrHoschi <mann_alexander2000@yahoo.de>
2026-09-03 09:25:12 +0200

    CR-06A rename visible build id

--- src/runtime/config.js
+++ src/runtime/config.js
@@ -1,6 +1,6 @@
 export const RuntimeConfig = Object.freeze({
   product: 'Neue Siedler',
-  build: 'CR-05-FREEZE',
+  build: 'CR-06A-TRANSPORT-EXECUTION-STATE-CONTRACT',
   simulation: Object.freeze({
     fixedStepMs: 100,
     maxCatchUpSteps: 4,
