01f2312810472d141f49d94479b0c4801d1c4543
DrHoschi <mann_alexander2000@yahoo.de>
2026-09-03 10:21:07 +0200

    CR-06C update visible build id

--- src/runtime/config.js
+++ src/runtime/config.js
@@ -1,6 +1,6 @@
 export const RuntimeConfig = Object.freeze({
   product: 'Neue Siedler',
-  build: 'CR-06B-PICKUP-EXECUTION-FOUNDATION',
+  build: 'CR-06C-DELIVERY-EXECUTION-FOUNDATION',
   simulation: Object.freeze({
     fixedStepMs: 100,
     maxCatchUpSteps: 4,
