7608fe6e208590c515d8ee8f93cd72291c369ee0
DrHoschi <mann_alexander2000@yahoo.de>
2026-09-03 11:07:10 +0200

    CR-06 set freeze gate build id

--- src/runtime/config.js
+++ src/runtime/config.js
@@ -1,6 +1,6 @@
 export const RuntimeConfig = Object.freeze({
   product: 'Neue Siedler',
-  build: 'CR-06C-DELIVERY-EXECUTION-FOUNDATION',
+  build: 'CR-06-FREEZE-GATE',
   simulation: Object.freeze({
     fixedStepMs: 100,
     maxCatchUpSteps: 4,
