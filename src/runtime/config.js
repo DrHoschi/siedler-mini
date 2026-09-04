1fe7b61d9bdd66ffb82c19a7c59c76223bc9f1d1
DrHoschi <mann_alexander2000@yahoo.de>
2026-09-04 08:45:28 +0200

    CR-14A: set cell occupancy contract build

--- src/runtime/config.js
+++ src/runtime/config.js
@@ -1,6 +1,6 @@
 export const RuntimeConfig = Object.freeze({
   product: 'Neue Siedler',
-  build: 'CR-13-DYNAMIC-ROUTE-VALIDITY-FOUNDATION-FREEZE-GATE',
+  build: 'CR-14A-CELL-OCCUPANCY-CONTRACT',
   simulation: Object.freeze({
     fixedStepMs: 100,
     maxCatchUpSteps: 4,
