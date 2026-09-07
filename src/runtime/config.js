729cd94482d1afb293e925d681517514bf6eb450
DrHoschi <mann_alexander2000@yahoo.de>
2026-09-07 21:45:52 +0200

    Start IM-14A player UI shell contract

--- src/runtime/config.js
+++ src/runtime/config.js
@@ -1,6 +1,6 @@
 export const RuntimeConfig = Object.freeze({
   product: 'Neue Siedler',
-  build: 'IM-13-WHOLE-BLOCK-COMPLETION-REGRESSION-FREEZE-GATE',
+  build: 'IM-14A-PLAYER-UI-SHELL-RESPONSIVE-SURFACE-CONTRACT',
   simulation: Object.freeze({
     fixedStepMs: 100,
     maxCatchUpSteps: 4,
