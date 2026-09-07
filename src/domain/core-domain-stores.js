db15f1dc37eb569a20686be447ffdc707d87e666
DrHoschi <mann_alexander2000@yahoo.de>
2026-09-07 18:30:58 +0200

    Add IM-13C CoreDomainStores restore initialization

--- src/domain/core-domain-stores.js
+++ src/domain/core-domain-stores.js
@@ -1,11 +1,25 @@
 import { DomainStore } from './domain-store.js';
 
 export class CoreDomainStores {
-  constructor() {
-    this.buildings = new DomainStore('buildings', 'building');
-    this.units = new DomainStore('units', 'unit');
-    this.resources = new DomainStore('resources', 'resource');
-    this.jobs = new DomainStore('jobs', 'transport-job');
+  constructor({ restoreDomains = null, allocators = null } = {}) {
+    const restored = restoreDomains ?? {};
+    const ids = allocators ?? {};
+    this.buildings = new DomainStore('buildings', 'building', {
+      allocator: ids.buildings ?? null,
+      restoreState: restored.buildings ?? null,
+    });
+    this.units = new DomainStore('units', 'unit', {
+      allocator: ids.units ?? null,
+      restoreState: restored.units ?? null,
+    });
+    this.resources = new DomainStore('resources', 'resource', {
+      allocator: ids.resources ?? null,
+      restoreState: restored.resources ?? null,
+    });
+    this.jobs = new DomainStore('jobs', 'transport-job', {
+      allocator: ids.jobs ?? null,
+      restoreState: restored.jobs ?? null,
+    });
     Object.freeze(this);
   }
 
