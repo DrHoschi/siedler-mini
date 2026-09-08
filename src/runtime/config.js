b90f21ddc14fdc4d47dbdfd91fd0facaf7bea427
DrHoschi <mann_alexander2000@yahoo.de>
2026-09-08 07:47:58 +0200

    Synchronize IM-14C runtime build identity

--- src/runtime/config.js
+++ src/runtime/config.js
@@ -1,6 +1,6 @@
 export const RuntimeConfig = Object.freeze({
   product: 'Neue Siedler',
-  build: 'IM-14B-UNIFIED-POINTER-TOUCH-INTERACTION-CONTRACT',
+  build: 'IM-14C-RUNTIME-HUD-PROJECTION',
   simulation: Object.freeze({
     fixedStepMs: 100,
     maxCatchUpSteps: 4,
