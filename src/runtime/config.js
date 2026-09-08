e7845b4f73929481316af2733f42516a3673f044
DrHoschi <mann_alexander2000@yahoo.de>
2026-09-08 11:29:22 +0200

    Synchronize IM-15B build identity

--- src/runtime/config.js
+++ src/runtime/config.js
@@ -1,6 +1,6 @@
 export const RuntimeConfig = Object.freeze({
   product: 'Neue Siedler',
-  build: 'IM-15A-INSPECTOR-SHELL-READ-ONLY-RUNTIME-OBSERVATION',
+  build: 'IM-15B-STRUCTURED-RUNTIME-DIAGNOSTICS-PROJECTION',
   simulation: Object.freeze({
     fixedStepMs: 100,
     maxCatchUpSteps: 4,
