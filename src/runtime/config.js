2f53f36cd3eae1f9a4bea5d9fa4565485b7ccdac
DrHoschi <mann_alexander2000@yahoo.de>
2026-09-09 16:37:13 +0200

    build: set IM-16G runtime identity

--- src/runtime/config.js
+++ src/runtime/config.js
@@ -1,6 +1,6 @@
 export const RuntimeConfig = Object.freeze({
   product: 'Neue Siedler',
-  build: 'IM-16F-PLAYER-BUILDING-SELECTION-PLACEMENT-ACTIVATION-CONTRACT',
+  build: 'IM-16G-AUTHORITATIVE-CONSTRUCTION-RESULT-PLAYER-UI-PROJECTION-CONTRACT',
   simulation: Object.freeze({
     fixedStepMs: 100,
     maxCatchUpSteps: 4,
