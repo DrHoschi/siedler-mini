d5dfe4e5787e20267fdc7abfb5af24da51a11f6a
DrHoschi <mann_alexander2000@yahoo.de>
2026-09-11 10:38:41 +0200

    chore: sync IM-17 whole-block visible build identity

--- src/runtime/config.js
+++ src/runtime/config.js
@@ -1,6 +1,6 @@
 export const RuntimeConfig = Object.freeze({
   product: 'Neue Siedler',
-  build: 'IM-17G-PLAYER-CONSTRUCTION-STATE-PROJECTION',
+  build: 'IM-17-WHOLE-BLOCK-COMPLETION-GATE',
   simulation: Object.freeze({
     fixedStepMs: 100,
     maxCatchUpSteps: 4,
