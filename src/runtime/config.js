75dd7dc5435bb7e15acf7d6ab70c409926011069
DrHoschi <mann_alexander2000@yahoo.de>
2026-09-18 17:00:41 +0200

    chore: identify IM-20F verification build

--- src/runtime/config.js
+++ src/runtime/config.js
@@ -1,6 +1,6 @@
 export const RuntimeConfig = Object.freeze({
   product: 'Neue Siedler',
-  build: 'IM-20E-BROWSER-SAVE-RELOAD-CONTINUE-LIFECYCLE-TESTBUILD-2',
+  build: 'IM-20F-EXACTLY-ONCE-RECOVERY-RECONCILIATION-TESTBUILD-1',
   simulation: Object.freeze({
     fixedStepMs: 100,
     maxCatchUpSteps: 4,
